2b8a6983cbbc0395a8986ec051c864272d631b51
mixxorz <mixxorz@gmail.com>
2015-04-01 22:41:38 +0800

    Updated setup.py to use find_packages()

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='behave-django',
     version='0.1.0',
-    packages=['behave_django'],
+    packages=find_packages(),
     include_package_data=True,
     license='MIT License',
     description=' Behave test runner for Django.',
