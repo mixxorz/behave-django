9b4a2cab6eac97bf01d69d3981c49069240f2720
mixxorz <mixxorz@gmail.com>
2015-04-01 22:11:51 +0800

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='behave-django',
+    version='0.1.0',
+    packages=['behave_django'],
+    include_package_data=True,
+    license='MIT License',
+    description=' Behave test runner for Django.',
+    long_description=README,
+    url='https://github.com/mixxorz/behave-django',
+    author='Mitchel Cabuloy',
+    author_email='mixxorz@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Testing',
+    ],
+)
