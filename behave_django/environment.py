96f0bffcc5dcdedf459e1312e5740b02fca9a0af
mixxorz <mixxorz@gmail.com>
2015-04-02 11:21:21 +0000

    Added example environment.py on the sample feature

--- behave_django/environment.py
+++ behave_django/environment.py
@@ -1,10 +1,10 @@
-from behave_django.testcases import BehaveDjangoTestCase
+from behave_django.testcase import BehaveDjangoTestCase
 
-def before_scenario(context):
+def before_scenario(context, scenario):
     context.test = BehaveDjangoTestCase()
     context.test.setUpClass()
     context.base_url = context.test.live_server_url
     
-def after_scenario(context):
+def after_scenario(context, scenario):
     context.test.tearDownClass()
     del context.test
