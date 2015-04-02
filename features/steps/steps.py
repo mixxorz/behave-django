a363ba107ac428cfd67658cd129c172b64cc6c92
mixxorz <mixxorz@gmail.com>
2015-04-02 13:47:47 +0000

    Added test for django assert library

--- features/steps/steps.py
+++ features/steps/steps.py
@@ -45,6 +45,11 @@ def use_assert_library(context):
     # If one of them works, all of them work. ;)
     context.test.assertEqual(1, 1)
     
+@when(u'I use the django assert library')
+def use_assert_library(context):
+    response = context.test.client.get('/')
+    context.test.assertTemplateUsed(response, 'index.html')
+    
 
 @then(u'it should work properly')
 def asserts_should_work(context):
