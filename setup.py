4809a9b886380531b90ae28622821c962cd6e531
mixxorz <mixxorz@gmail.com>
2015-04-04 05:17:29 +0800

    Bumped version 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='behave-django',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(exclude=['test*']),
     include_package_data=True,
     license='MIT License',
