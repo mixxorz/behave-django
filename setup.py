69ea416f47aef9bc41a157b492190eee8f54bb84
mixxorz <mixxorz@gmail.com>
2015-04-04 03:59:41 +0800

    Fixed setup.py to not include test apps and projects

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='behave-django',
     version='0.1.0',
-    packages=find_packages(),
+    packages=find_packages(exclude=['test*']),
     include_package_data=True,
     license='MIT License',
     description='Behave BDD integration for Django',
