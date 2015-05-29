bcc344adddb6f06ad1c1698c2174c81bcea7986a
Peter Bittner <django@bittner.it>
2015-05-29 03:15:53 +0200

    Add get_url() with built-in reverse() to context
    Resolves issue #10

--- behave_django/environment.py
+++ behave_django/environment.py
@@ -1,4 +1,5 @@
 from django.core.management import call_command
+from django.core.urlresolvers import reverse as url_reverse
 
 from behave_django.testcase import BehaveDjangoTestCase
 
@@ -16,6 +17,26 @@ def before_scenario(context, scenario):
         call_command('loaddata', *context.fixtures, verbosity=0)
 
     context.base_url = context.test.live_server_url
+
+    def get_url(path=None, reverse=None):
+        """
+        URL helper attached to context with built-in reverse resolution as a
+        handy shortcut. Takes either a path or a reverse name.  Examples::
+
+            context.get_url()
+            context.get_url('/path/to/page/')
+            context.get_url(reverse='resource-name')
+            context.get_url(reverse('resource-name'))
+            context.get_url(reverse('resource-name', 'args', and='kwargs'))
+        """
+        if path is not None:
+            return context.base_url + path
+        elif reverse is not None:
+            return context.base_url + url_reverse(reverse)
+        else:
+            return context.base_url
+
+    context.get_url = get_url
 
 
 def after_scenario(context, scenario):
