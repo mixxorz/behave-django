5566c15b8c504668ef8f89efb6e20e381dd22db5
Peter Bittner <django@bittner.it>
2015-06-03 16:00:55 +0200

    Temporarily deactivate stderr redirection only
    Debugging Python 2.6 + Django 1.6 fail on Travis-CI

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-# import os
+import os
 import subprocess
 import unittest
 
@@ -6,19 +6,21 @@ import unittest
 class BehaveDjangoTestCase(unittest.TestCase):
 
     def test_command_should_exit_zero_if_passing(self):
-        # FNULL = open(os.devnull, 'w')
+        FNULL = open(os.devnull, 'w')
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '~@failing'],
-            # stdout=FNULL,
-            stderr=subprocess.STDOUT)
+            stdout=FNULL,
+            # stderr=subprocess.STDOUT,
+            )
         self.assertEqual(return_code, 0)
 
     def test_command_should_exit_nonzero_if_failing(self):
-        # FNULL = open(os.devnull, 'w')
+        FNULL = open(os.devnull, 'w')
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '@failing'],
-            # stdout=FNULL,
-            stderr=subprocess.STDOUT)
+            stdout=FNULL,
+            # stderr=subprocess.STDOUT,
+            )
         self.assertNotEqual(return_code, 0)
 
 if __name__ == '__main__':
