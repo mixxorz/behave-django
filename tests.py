3057be090249f40e6bcdf1a469b55d22b6318978
Peter Bittner <django@bittner.it>
2015-06-03 13:59:37 +0200

    Revert disabled redirection, binary open /dev/null

--- tests.py
+++ tests.py
@@ -6,21 +6,19 @@ import unittest
 class BehaveDjangoTestCase(unittest.TestCase):
 
     def test_command_should_exit_zero_if_passing(self):
-        # FNULL = open(os.devnull, 'w')
+        FNULL = open(os.devnull, 'wb')
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '~@failing'],
-            # stdout=FNULL,
-            # stderr=subprocess.STDOUT,
-            )
+            stdout=FNULL,
+            stderr=subprocess.STDOUT)
         self.assertEqual(return_code, 0)
 
     def test_command_should_exit_nonzero_if_failing(self):
-        # FNULL = open(os.devnull, 'w')
+        FNULL = open(os.devnull, 'wb')
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '@failing'],
-            # stdout=FNULL,
-            # stderr=subprocess.STDOUT,
-            )
+            stdout=FNULL,
+            stderr=subprocess.STDOUT)
         self.assertNotEqual(return_code, 0)
 
 if __name__ == '__main__':
