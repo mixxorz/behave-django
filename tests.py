9224184e21fde581d13bf2ea93a7acce9ce882b6
Peter Bittner <django@bittner.it>
2015-06-03 15:36:48 +0200

    Temporarily deactivate stdout redirection only
    Debugging Python 2.6 + Django 1.6 fail on Travis-CI

--- tests.py
+++ tests.py
@@ -1,4 +1,4 @@
-import os
+# import os
 import subprocess
 import unittest
 
@@ -6,18 +6,18 @@ import unittest
 class BehaveDjangoTestCase(unittest.TestCase):
 
     def test_command_should_exit_zero_if_passing(self):
-        FNULL = open(os.devnull, 'wb')
+        # FNULL = open(os.devnull, 'w')
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '~@failing'],
-            stdout=FNULL,
+            # stdout=FNULL,
             stderr=subprocess.STDOUT)
         self.assertEqual(return_code, 0)
 
     def test_command_should_exit_nonzero_if_failing(self):
-        FNULL = open(os.devnull, 'wb')
+        # FNULL = open(os.devnull, 'w')
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '@failing'],
-            stdout=FNULL,
+            # stdout=FNULL,
             stderr=subprocess.STDOUT)
         self.assertNotEqual(return_code, 0)
 
