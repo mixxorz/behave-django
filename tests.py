464f769bf4286f29c6254ae766a8b6ba5d1f81c1
Peter Bittner <django@bittner.it>
2015-06-03 16:51:39 +0200

    Restore original tests.py (failing in Travis-CI)

--- tests.py
+++ tests.py
@@ -10,8 +10,7 @@ class BehaveDjangoTestCase(unittest.TestCase):
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '~@failing'],
             stdout=FNULL,
-            # stderr=subprocess.STDOUT,
-            )
+            stderr=subprocess.STDOUT)
         self.assertEqual(return_code, 0)
 
     def test_command_should_exit_nonzero_if_failing(self):
@@ -19,8 +18,7 @@ class BehaveDjangoTestCase(unittest.TestCase):
         return_code = subprocess.call(
             ['python', 'manage.py', 'behave', '--tags', '@failing'],
             stdout=FNULL,
-            # stderr=subprocess.STDOUT,
-            )
+            stderr=subprocess.STDOUT)
         self.assertNotEqual(return_code, 0)
 
 if __name__ == '__main__':
