83bd637acd2b500e12de2fc5fd4f153e95e245ea
mixxorz <mixxorz@gmail.com>
2015-06-10 00:47:56 +0800

    Added "Fork me on GitHub" banner

--- docs/conf.py
+++ docs/conf.py
@@ -116,7 +116,12 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'github_user': 'mixxorz',
+    'github_repo': 'behave-django',
+    'github_banner': 'true',
+}
+
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
