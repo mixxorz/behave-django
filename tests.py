eff7b5b0318891e4d91395404503cd243fa28e22
Mitchel Cabuloy <mitch@kaijucode.io>
2015-07-11 14:11:12 +0800

    Fixed import issue with Command

--- tests.py
+++ tests.py
@@ -14,8 +14,6 @@ import django
 import os
 import unittest
 
-from behave_django.management.commands.behave import Command
-
 
 def run_silently(command):
     """Run a shell command and return both exit_status and console output."""
@@ -94,6 +92,7 @@ class BehaveDjangoTestCase(unittest.TestCase):
         mock_existing_database_runner.assert_called_once_with()
 
     def test_should_accept_behave_arguments(self):
+        from behave_django.management.commands.behave import Command
         command = Command()
         args = command.get_behave_args(
             argv=['manage.py', 'behave',
@@ -105,6 +104,7 @@ class BehaveDjangoTestCase(unittest.TestCase):
         assert 'progress' in args
 
     def test_should_not_include_non_behave_arguments(self):
+        from behave_django.management.commands.behave import Command
         command = Command()
         args = command.get_behave_args(
             argv=['manage.py', 'behave',
@@ -116,6 +116,7 @@ class BehaveDjangoTestCase(unittest.TestCase):
         assert 'test_project.settings' not in args
 
     def test_should_return_positional_args(self):
+        from behave_django.management.commands.behave import Command
         command = Command()
         args = command.get_behave_args(
             argv=['manage.py', 'behave',
@@ -126,6 +127,7 @@ class BehaveDjangoTestCase(unittest.TestCase):
         assert 'features/running-tests.feature' in args
 
     def test_no_arguments_should_not_cause_issues(self):
+        from behave_django.management.commands.behave import Command
         command = Command()
         args = command.get_behave_args(
             argv=['manage.py', 'behave'])
