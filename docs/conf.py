ade734b18566e2c4bb1a669701782e984630829d
Mitchel Cabuloy <mitch@kaijucode.io>
2015-08-16 15:15:31 +0800

    Update authors and copyright in docs

--- docs/conf.py
+++ docs/conf.py
@@ -50,8 +50,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'behave-django'
-copyright = u'2015, Mitchel Cabuloy, Peter Bittner'
-author = u'Mitchel Cabuloy, Peter Bittner'
+copyright = u'2015, Mitchel Cabuloy'
+author = u'Mitchel Cabuloy'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -231,7 +231,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
   (master_doc, 'behave-django.tex', u'behave-django Documentation',
-   u'Mitchel Cabuloy, Peter Bittner', 'manual'),
+   u'Mitchel Cabuloy', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
